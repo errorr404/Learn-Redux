4c9d78a011d8343f4624c89af497b7f1542b96a2
errorr404 <dkbishwash@gmail.com>
2018-07-01 07:04:39 +0530

    adding the feature of cookies from adding the library sfcookies(npm install sfcookies --save) like read_cookie, bake_cooking, we can use it insted of local storage

--- Reminder_App/src/reducers/index.js
+++ Reminder_App/src/reducers/index.js
@@ -1,5 +1,5 @@
 import { ADD_REMINDER, DELETE_REMINDER } from '../constant';
-
+import { bake_cookie, read_cookie} from 'sfcookies'
 
 const reminder = (action)=>{
   let { text,dueDate } = action;
@@ -19,14 +19,17 @@ const removeById = (state=[],id) =>{
 
 const reminders = (state=[],action)=>{
   let reminders = null;
+  state = read_cookie('reminders');
   switch (action.type) {
     case ADD_REMINDER:
       reminders = [...state,reminder(action)];
+      bake_cookie('reminders',reminders);
       console.log('reminders as state',reminders);
       return reminders;
 
       case DELETE_REMINDER:
         reminders = removeById(state,action.id);
+        bake_cookie('reminders',reminders);
         return reminders;
     default:
     return state;
